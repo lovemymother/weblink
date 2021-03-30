69ec0e008c2f9ed4c0b2a6c9d6fc15b3c4e6b427
lovemymother <78233860+lovemymother@users.noreply.github.com>
2021-03-30 19:54:49 +0700

    Create firebase-config.js

---
+++ src/config/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBjSLDpgR18Pdk1FTtUGJAqf9iMUPVCC4k",
+    authDomain: "roma-link.appspot.com",
+    databaseURL: "https://roma-link-default-rtdb.firebaseio.com",
+    projectId: "roma-link",
+    storageBucket: "roma-link.appspot.comT",
+    messagingSenderId: "188062853008",
+    appId: "1:188062853008:web:bf51786de4683e5b463515",
+    measurementId: "G-9TT27R5H7V"
+};
